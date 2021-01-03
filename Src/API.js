bb149450c95cafa2f6730182d5e1b31fa59ef04e
tamer adel <tameradel209@icloud.com>
2021-01-03 15:34:23 +0200

    complete

--- Src/API.js
+++ Src/API.js
@@ -1,6 +1,6 @@
 export const fetchData = async (searchWord) =>{
     try{
-        const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${searchWord}&type=video&key=AIzaSyCnsNC_wrsXAOqv9nk6LM9UDdqscPqTL2U`)
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${searchWord}&type=video&key=AIzaSyAu6voOneUabcyITQWrpqcIL2Zzbpfbrfg`)
         const data = await response.json()
         const items = data.items
         return items
@@ -8,7 +8,7 @@ export const fetchData = async (searchWord) =>{
         catch(err){throw new Error(err)}
 }
 
-export const autoComplete =async () =>{
+export const autoComplete =async (query) =>{
     try{
         const response = await fetch(`http://suggestqueries.google.com/complete/search?client=chrome&q=${query}`)
         const data = await response.json()
